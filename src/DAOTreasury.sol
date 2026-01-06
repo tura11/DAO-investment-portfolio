9639a351c85a81fe6522f7e1509fb6c374f21f86
Tymek <tymek_uramek@wp.pl>
2026-01-06 18:13:33 +0100

    DaoTreasury finished

--- src/DAOTreasury.sol
+++ src/DAOTreasury.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract DAOTreasury is ERC20, Ownable {
 
     error DAOTreasury__DepositTooSmall();
+    error DAOTreasury__NotEnoughTokens();
 
     uint256 public constant MIN_DEPOSIT = 0.001 ether;
 
@@ -22,7 +23,7 @@ contract DAOTreasury is ERC20, Ownable {
 
     function deposit() external  payable {
         if(msg.value < MIN_DEPOSIT) {
-            revert DepositToDAOTreasury__DepositTooSmalloSmall();
+            revert DepositToDAOTreasury__DepositTooSmall();
         }
         uint256 tokensToMint = msg.value * 1000;
         totalDeposits += msg.value;
@@ -30,6 +31,25 @@ contract DAOTreasury is ERC20, Ownable {
         _mint(msg.sender, tokensToMint);
 
         emit Deposited(msg.sender, msg.value, tokensToMint);
+    }
+
+
+    function withdraw(uint256 tokenAmount) external {
+        if(balanceOf(msg.sender) < tokenAmount) {
+            revert DAOTreasury__NotEnoughTokens();
+        }
+
+        uint256 ethToReceive = (tokenAmount * address(this).balance) / totalSupply();
+
+        _burn(msg.sender, tokenAmount);
+        totalDeposits -= ethToReceive;
+
+        emit Withdrawn(msg.sender, tokenAmount, ethToReceive);
+    }
+
+
+    function getTreasuryBalance() external view returns (uint256) {
+        return address(this).balance;
     }
 
 
