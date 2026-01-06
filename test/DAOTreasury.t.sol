24a8979dd1442b9331435f3bab839eddc1f8fe94
Tymek <tymek_uramek@wp.pl>
2026-01-06 19:17:46 +0100

    100% coverage

--- test/DAOTreasury.t.sol
+++ test/DAOTreasury.t.sol
@@ -19,6 +19,9 @@ contract DAOTreasuryTest is Test {
         vm.deal(user2, 100 ether);
 
     }
+    //////////////////////////////
+    //      TESTING DEPOSIT     //
+    //////////////////////////////
 
     function testDeposit() public {
         vm.prank(user1);
@@ -28,21 +31,109 @@ contract DAOTreasuryTest is Test {
         assertEq(treasury.getTotalTokensMinted(), 1000 ether);
     }
 
-function testWithdraw() public {
-    vm.prank(user1);
-    treasury.deposit{value: 1 ether}();
+    function testDepositRevertIfDepositTooSmall() public {
+        vm.prank(user1);
+        vm.expectRevert(DAOTreasury.DAOTreasury__DepositTooSmall.selector);
+        treasury.deposit{value: 0.0001 ether}();
+    }
+
+    function testDepositEmitsEvent() public {
+        vm.prank(user1);
+        vm.expectEmit(true, false, false, true);
+        emit DAOTreasury.Deposited(user1, 1 ether, 1000 ether);
+        treasury.deposit{value: 1 ether}();
+    }
+
+     //////////////////////////////
+    //      TESTING WITHDRAW    //
+    //////////////////////////////
+    function testWithdraw() public {
+        vm.prank(user1);
+        treasury.deposit{value: 1 ether}();
 
 
-    uint256 balanceBefore = user1.balance;
+        uint256 balanceBefore = user1.balance;
+        
+        vm.prank(user1);
+        treasury.withdraw(500 ether);
+        
+
+        uint256 balanceAfter = user1.balance;
+
+        // Assertions
+        assertEq(treasury.balanceOf(user1), 500 ether);
+        assertEq(balanceAfter - balanceBefore, 0.5 ether);
+    }
+
+
+     function testMultipleDepositsAndWithdrawals() public {
+   
+        vm.prank(user1);
+        treasury.deposit{value: 1 ether}();
+        
+     
+        vm.prank(user2);
+        treasury.deposit{value: 2 ether}();
+        
+        // Total: 3 ETH in treasury
+        // user1 has: 1000 tokens (33.33%)
+        // user2 has: 2000 tokens (66.67%)
+        
+        assertEq(treasury.getTreasuryBalance(), 3 ether);
+        assertEq(treasury.balanceOf(user1), 1000 ether);
+        assertEq(treasury.balanceOf(user2), 2000 ether);
+        
+
+        vm.prank(user1);
+        treasury.withdraw(1000 ether);
+        
+
+        assertEq(treasury.getTreasuryBalance(), 2 ether);
+    }
     
-    vm.prank(user1);
-    treasury.withdraw(500 ether);
-    
+    function testProportionalWithdrawal() public {
+        vm.prank(user1);
+        treasury.deposit{value: 1 ether}();
+        
+        vm.prank(user2);
+        treasury.deposit{value: 1 ether}();
+        
+   
+        assertEq(treasury.balanceOf(user1), 1000 ether);
+        assertEq(treasury.balanceOf(user2), 1000 ether);
+        
+   
+        vm.prank(user1);
+        treasury.withdraw(1000 ether);
+        
 
-    uint256 balanceAfter = user1.balance;
+        assertEq(treasury.getTreasuryBalance(), 1 ether);
+        assertEq(treasury.balanceOf(user1), 0);
+    }
+    function testWithdrawRevertIfNotEnoughTokens() public {
+        vm.prank(user1);
+        vm.expectRevert(DAOTreasury.DAOTreasury__NotEnoughTokens.selector);
+        treasury.withdraw(1 ether);
+    }
+    function testWithdrawRevertIfInsufficientFunds() public {
+        vm.prank(user1);
+        treasury.deposit{value: 1 ether}();
 
-    // Assertions
-    assertEq(treasury.balanceOf(user1), 500 ether);
-    assertEq(balanceAfter - balanceBefore, 0.5 ether);
+        vm.deal(address(treasury), 0);
+
+        vm.prank(user1);
+        vm.expectRevert(DAOTreasury.DAOTreasury__InsufficientFunds.selector);
+        treasury.withdraw(1000 ether);
+    }
+
+    function testWithdrawEmitsEvent() public {
+        vm.prank(user1);
+        treasury.deposit{value: 1 ether}();
+
+        vm.prank(user1);
+        vm.expectEmit(true, false, false, true);
+        emit DAOTreasury.Withdrawn(user1, 1000 ether, 1 ether);
+        treasury.withdraw(1000 ether);
+    }
 }
-}
+
