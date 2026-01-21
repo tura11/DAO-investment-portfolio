19a8bc5c7d995ede15deb20a16778966f6db0486
Tymek <tymek_uramek@wp.pl>
2026-01-21 20:54:18 +0100

    FEAT: Interactions.s.sol

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -106,3 +106,103 @@ contract VoteOnProposal is Script{
         voteOnProposal(mostRecentlyDeployed, 0, DAOGovernance.VoteType.For);
     }
 }
+
+
+contract FinalizeProposal is Script {
+    function finalizeProposal(address governanceAddress, uint256 proposalId) public {
+        vm.startBroadcast();
+        DAOGovernance governance = DAOGovernance(governanceAddress);
+        governance.finalizeProposal(proposalId);
+        vm.stopBroadcast();
+        console.log("Finalized proposal #%s", proposalId);
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("DAOGovernance", block.chainid);
+        finalizeProposal(mostRecentlyDeployed, 0);
+    }
+}
+
+
+contract ExecuteProposal is Script {
+    function executeProposal(address governanceAddress, uint256 proposalId) public {
+        vm.startBroadcast();
+        DAOGovernance governance = DAOGovernance(governanceAddress);
+        governance.executeProposal(proposalId);
+        vm.stopBroadcast();
+        console.log("Executed proposal #%s", proposalId);
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("DAOGovernance", block.chainid);
+        executeProposal(mostRecentlyDeployed, 0);
+    }
+}
+
+contract CancelProposal is Script {
+    function cancelProposal(address governanceAddress, uint256 proposalId) public {
+        vm.startBroadcast();
+        DAOGovernance governance = DAOGovernance(governanceAddress);
+        governance.cancelProposal(proposalId);
+        vm.stopBroadcast();
+        console.log("Canceled proposal #%s", proposalId);
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("DAOGovernance", block.chainid);
+        cancelProposal(mostRecentlyDeployed, 0);
+    }
+}
+
+
+contract GetProposalInfo is Script {
+    function getProposalInfo(address governanceAddress, uint256 proposalId) public view {
+        DAOGovernance governance = DAOGovernance(governanceAddress);
+        DAOGovernance.ProposalView memory proposal = governance.getProposal(proposalId);
+        
+        console.log("===========================================");
+        console.log("PROPOSAL #%s", proposalId);
+        console.log("===========================================");
+        console.log("Title:        %s", proposal.title);
+        console.log("Description:  %s", proposal.description);
+        console.log("Proposer:     %s", proposal.proposer);
+        console.log("Target:       %s", proposal.targetContract);
+        console.log("ETH Amount:   %s", proposal.ethAmount);
+        console.log("-------------------------------------------");
+        console.log("Votes For:     %s", proposal.votesFor);
+        console.log("Votes Against: %s", proposal.votesAgainst);
+        console.log("Votes Abstain: %s", proposal.votesAbstain);
+        console.log("-------------------------------------------");
+        console.log("Start Time:    %s", proposal.startTime);
+        console.log("End Time:      %s", proposal.endTime);
+        console.log("Execution:     %s", proposal.executionTime);
+        console.log("State:         %s", uint256(proposal.state));
+        console.log("===========================================");
+    }
+
+    function run() external view {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("DAOGovernance", block.chainid);
+        getProposalInfo(mostRecentlyDeployed, 0);
+    }
+}
+
+contract GetTreasuryInfo is Script {
+    function getTreasuryInfo(address treasuryAddress) public view {
+        DAOTreasury treasury = DAOTreasury(payable(treasuryAddress));
+        
+        console.log("===========================================");
+        console.log("TREASURY INFO");
+        console.log("===========================================");
+        console.log("Treasury Address: %s", treasuryAddress);
+        console.log("ETH Balance:      %s", treasury.getTreasuryBalance());
+        console.log("Total Supply:     %s", treasury.getTotalTokensMinted());
+        console.log("Total Deposits:   %s", treasury.totalDeposits());
+        console.log("Governance:       %s", treasury.governance());
+        console.log("===========================================");
+    }
+
+    function run() external view {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("DAOTreasury", block.chainid);
+        getTreasuryInfo(mostRecentlyDeployed);
+    }
+}
